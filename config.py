54da9ee5d485f35c1caa38d2a33eaa6c584f583e
luffy7254 <redmiop2211@gmail.com>
2025-08-23 21:17:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("22532815"))
+API_HASH = os.getenv("cdc905788c22458df1276e488c6d19b2")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+SESSION = os.getenv("SESSION", "AQE1hZwAKug_GOBdXMCf-4THWQxOY57RZehQRPgLzHw4bFG5e2rX_pK5epOe61MZ16rjxjxKH_a3eFEKsl4JK5db76Pcu2-uo2imgn-gbPARCkr7_7XkYmooaEg76QyVrf225-IRaDWk343cVIaCP-aYwIbCrYGgFgkBCa_-IO0ojtuUN5kDnhwA3wg0EygW8sTW6sLK-zSHX5VtrzSz4_uXulNc0OW1zS2eTgHd0K1Ke28prBeekrBcSjBsP3omZdNdvKV4zl6r2i2QEs9zdWfdpCjYBKgnacELqiRMMhkUnZjMdk_kDZtYjllWIKDQkkB0mVa9gi22c545-GlO1UVSMa4IoQAAAAHDHIcwAA")
+TIME = int(os.getenv("TIME", "10"))  # Seconds before deletion
